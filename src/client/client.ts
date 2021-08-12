103da0e14690f737cc55cd101ada05d3400084e6
rhum <lucas.rioust@gmail.com>
2021-08-12 13:53:03 +0200

    Progress

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,46 +1,136 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+//#region IMPORTS
+import {
+    PerspectiveCamera,
+    PointLight,
+    Scene,
+    Vector3,
+    WebGLRenderer,
+    BoxGeometry,
+    Matrix4,
+    MeshLambertMaterial, Mesh, Object3D, BufferGeometry, MeshPhongMaterial, Clock
+} from "three";
+import {BufferGeometryUtils} from "three/examples/jsm/utils/BufferGeometryUtils";
+//#endregion
 
-const scene = new THREE.Scene()
+//#region GLOBAL VARIABLES
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 2
+let camera : PerspectiveCamera;
+let scene : Scene
+let renderer : WebGLRenderer
+let clock : Clock
+let deltaTime : number
+let width : number
+let height : number
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+//#endregion
 
-const controls = new OrbitControls(camera, renderer.domElement)
-
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
-
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
-}
-
+//#region Update
 function animate() {
+    deltaTime = clock.getDelta()
     requestAnimationFrame(animate)
-
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
-
-    controls.update()
-
+    
+    AnimateScene1()
     render()
 }
 
 function render() {
     renderer.render(scene, camera)
 }
-animate()
+//#endregion
+
+//#region Start
+const Start = () => {
+    clock = new Clock()
+    clock.start()
+    InitScene()
+    InitEventListeners()
+    CreateScene1()
+    animate()
+}
+
+const InitScene = () => {
+    scene = new Scene()
+    
+    width = window.innerWidth
+    height = window.innerHeight
+    
+    camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    camera.position.z = 2
+
+    renderer = new WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+}
+
+const InitEventListeners = () => {
+    window.addEventListener('resize', () => {
+        width = window.innerWidth
+        height = window.innerHeight
+        
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+        renderer.setSize(window.innerWidth, window.innerHeight)
+        render()
+    }, false)
+}
+//#endregion
+
+//#region SCENE 1
+
+//#region SCENE 1 Parameters 
+
+let cameraTarget : Vector3
+
+//#endregion
+
+const CreateScene1 = () => {
+
+    camera = new PerspectiveCamera( 60, width / height, 1, 4000 )
+    cameraTarget = new Vector3()
+    scene = new Scene()
+
+    // LIGHTS
+    const light = new PointLight( 0xff0000, 5, 300 )
+    scene.add( light )
+    const light1 = new PointLight( 0x8844ff, 5, 1000 )
+    scene.add( light1 )
+
+    // CITY
+    let cube = new BoxGeometry( 2, 2, 2 )
+    let material = new MeshLambertMaterial( {
+        color: 0x808080
+    } );
+
+    for ( let i = 0; i < 800; i ++ ) {
+        let object = new Mesh(cube, material)
+        object.position.x = Math.random() * 2000 - 1000
+        object.position.z = Math.random() * 2000 - 1000
+        object.scale.x = Math.random() * 20
+        object.scale.y = Math.random() * Math.random() * 100
+        object.scale.z = Math.random() * 20
+        scene.add(object)
+    }
+    material = new MeshPhongMaterial( {
+        color: 0x606060,
+        wireframe: true
+    } )
+
+    for ( let i = 0; i < 800; i ++ ) {
+        let object = new Mesh(cube, material)
+        object.position.x = Math.random() * 2000 - 1000
+        object.position.z = Math.random() * 2000 - 1000
+        object.scale.x = Math.random() * 20
+        object.scale.y = Math.random() * Math.random() * 100
+        object.scale.z = Math.random() * 20
+        scene.add(object)
+    }
+    
+    camera.position.set(0,0,0)
+}
+
+const AnimateScene1 = () => {
+    camera.position.z += deltaTime * 10
+}
+//#endregion
+
+Start()
